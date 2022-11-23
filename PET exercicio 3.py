b970433bf503b871ea7ef45adc6a52d4fdddcfcd
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-11-23 07:20:32 -0300

    Add files via upload

---
+++ PET exercicio 3.py
@@ -0,0 +1,7 @@
+tempo = int(input("quanto tempo?"))
+velocidade = int(input("qual a velocidade media?"))
+distancia = tempo * velocidade
+litros_usados = distancia / 12
+
+print(print("tempo:", tempo, "velocidade:", velocidade, "distancia:", distancia, "litros usados:"),
+litros_usados)
