0e54777b25bd652581f64064e051c7de5dc207c7
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-11-23 07:10:59 -0300

    Add files via upload

---
+++ PET exercicio 2.py
@@ -0,0 +1,12 @@
+idade = int(input("digite sua idade"))
+
+print(idade)
+
+if(idade <= 12):
+    print("criança")
+elif(idade >= 13 and idade <= 17):
+    print("adolescente")
+elif(idade >= 18):
+    print("adulto")
+
+
